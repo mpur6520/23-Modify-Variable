8aef7d633bf252dd6191fea698cc99a86b4c679a
Maru Puran <144689560+mpur6520@users.noreply.github.com>
2024-03-17 19:25:38 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,14 @@
+# Task Modify
+
+Adapt the code to output your own message. 
+Add comments to show what it does.
+
+## Help! My Code Doesn't Work!
+
+Make sure that you check for the following things:
+
+- print does not have a capital letter
+- The string text is surrounded with speech marks
+- The string (including speech marks) is surrounded by brackets.
+- Variable goes on the left, data on the right for assignment.
+- You've used a '+' between each string and variable that need concatenating.
---
+++ main.py
@@ -0,0 +1,16 @@
+# Variable Assignment - Modify
+#modify a code to add more variables and output them, which store strings
+
+#You can combine variables with strings in an output
+
+name1 = "Axl"
+name2 = "Slash"
+#Add 2 more variables to store 'Duff' and 'Izzy'
+name3 = "Duff" #create variable called name3 to store string "Duff"
+name4 = "Izzy" #create variable called name4 to store string "Izzy"
+
+#This line uses concatenation to join the variables together with the string 'and' to make a sentence.
+
+#Complete the line to output all of the variables 
+print(name1 + " and " + name2 + " and " + name3 + " and " + name4 + ".")
+#use + signs to concatenate all strings and variables together to form one sentence to be outputted.
